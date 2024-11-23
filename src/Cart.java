502304e7029d1716132180a85102b806115ce859
Patrick_Hieu <hieunguyenthanh204@gmail.com>
2024-11-23 11:07:32 +0700

    Fixed Conflict

--- src/Cart.java
+++ src/Cart.java
@@ -13,6 +13,37 @@ public class Cart {
         }
     }
 
+    public void addDigitalVideoDisc(DigitalVideoDisc[] dvdList) {
+        for (DigitalVideoDisc dvd : dvdList) {
+            if (itemOrdered < 20) {
+                items[itemOrdered] = dvd;
+                itemOrdered++;
+                System.out.println("Completed Added DVD: " + dvd.getTitle());
+            } else {
+                System.out.println("The cart is almost full.");
+                break;
+            }
+        }
+    }
+
+    public void addDigitalVideoDisc(DigitalVideoDisc dvd1, DigitalVideoDisc dvd2) {
+        if (itemOrdered < 20) {
+            items[itemOrdered] = dvd1;
+            itemOrdered++;
+            System.out.println("Completed Added DVD: " + dvd1.getTitle());
+        } else {
+            System.out.println("The cart is almost full.");
+        }
+
+        if (itemOrdered < 20) {
+            items[itemOrdered] = dvd2;
+            itemOrdered++;
+            System.out.println("Completed Added DVD: " + dvd2.getTitle());
+        } else {
+            System.out.println("The cart is almost full.");
+        }
+    }
+
     // Print list of Cart
     public void printCart() {
         System.out.println("***********************CART***********************");
